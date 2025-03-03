e4157fb79e23cbfff9a7b8932cdb96ed72b72592
MikhailZolotarev2024 <mikhailzolotarev2018@gmail.com>
2025-03-03 11:44:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -10,4 +10,26 @@ function toggleMenu() {
             const section = event.target.parentElement;
             section.classList.toggle('active');
         }
+
+document.getElementById("contactForm").addEventListener("submit", async function(event) {
+    event.preventDefault(); // Предотвращаем стандартную отправку формы
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    try {
+        const response = await fetch("https://9a7d-185-244-159-19.ngrok-free.app/send", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, email, message })
+        });
+
+        const result = await response.json();
+        alert(result.success || result.error);
+    } catch (error) {
+        console.error("Ошибка при отправке формы:", error);
+        alert("Ошибка при отправке. Проверьте соединение с сервером.");
+    }
+}
     </script>
